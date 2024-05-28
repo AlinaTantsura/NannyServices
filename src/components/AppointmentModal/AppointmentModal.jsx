8d4bd604022f48a928c51aeff9440ddee60cbaca
Alina <alinatanst@gmail.com>
2024-05-28 20:46:35 +0300

    Add custom time input

--- src/components/AppointmentModal/AppointmentModal.jsx
+++ src/components/AppointmentModal/AppointmentModal.jsx
@@ -10,28 +10,49 @@ import {
 import ModalWindow from "../ModalWindow/ModalWindow";
 import {
   AddNameText,
-  InpunFullAppoinmentBox,
   InputAppointmentBox,
+  InputWrapBox,
   NameBlock,
   NannyInfoBox,
   PhotoOfNanny,
+  TimeListStyled,
+  TimeListStyledBox,
 } from "./AppointmentModal.styled";
 import Button from "../Button/Button";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
 const AppointmentFormValidateSchema = yup.object().shape({
-  address: yup.string().trim().min(3).required("Address is required"),
-  phone: yup.string().length(13).required("Phone is required"),
-  childAge: yup.number().required("Child's age is required"),
+  address: yup
+    .string()
+    .trim()
+    .min(3, "Address must have at least 3 characters")
+    .required("Address is required"),
+  phone: yup
+    .string()
+    .length(13, "Phone number must be exactly 13 characters")
+    .required("Phone is required"),
+  childAge: yup
+    .number("Child's age must be a number")
+    .required("Child's age is required")
+    .max(18, "Child's age must be less than 18"),
   time: yup.string().required("Time is required"),
   email: yup
     .string()
     .trim()
     .required("Email is required")
     .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, "Email is not valid"),
-  name: yup.string().trim().min(2).required("Name is required"),
-  comment: yup.string().trim().min(2).required("Comment is required"),
+  name: yup
+    .string()
+    .trim()
+    .required("Name is required")
+    .min(2, "Name must have at least 2 characters"),
+  comment: yup
+    .string()
+    .trim()
+    .required("Comment is required")
+    .min(2, "Comment must have at least 2 characters"),
 });
 
 const AppointmentModal = ({ open, onClose }) => {
@@ -41,13 +62,21 @@ const AppointmentModal = ({ open, onClose }) => {
     reset,
     formState: { errors },
   } = useForm({ resolver: yupResolver(AppointmentFormValidateSchema) });
+  const [isOpenTimeDrop, setIsOpenTimeDrop] = useState(false);
+  const [timeValue, setTimeValue] = useState("00:00");
 
   const handleSubmitForm = (data) => {
     console.log(data);
-
     reset();
     onClose(true);
   };
+
+  const handleTimeCheck = (e) => {
+    if (e.target !== e.currentTarget) {
+      setIsOpenTimeDrop(false);
+      return setTimeValue(e.target.innerText.replaceAll(" ", ""))
+    }
+  }
   console.log(errors);
   return (
     <ModalWindow open={open} onClose={onClose}>
@@ -66,34 +95,99 @@ const AppointmentModal = ({ open, onClose }) => {
           </NameBlock>
         </NannyInfoBox>
         <InputAppointmentBox>
-      
+          <InputWrapBox>
             <InputStyled
               type="text"
               {...register("address")}
               placeholder="Address"
             />
-           {errors.address &&  <ErrorMessage>{errors.address?.message}</ErrorMessage>}
-         
+            {errors.address && (
+              <ErrorMessage>{errors.address?.message}</ErrorMessage>
+            )}
+          </InputWrapBox>
+          <InputWrapBox>
             <InputStyled
               type="tel"
               {...register("phone")}
               placeholder="Phone"
               defaultValue="+380"
             />
-            {errors.phone && <ErrorMessage>{errors.phone?.message}</ErrorMessage>}
-       
+            {errors.phone && (
+              <ErrorMessage>{errors.phone?.message}</ErrorMessage>
+            )}
+          </InputWrapBox>
+          <InputWrapBox>
             <InputStyled
               type="text"
               {...register("childAge")}
               placeholder="Child's age"
             />
-            {errors.childAge && <ErrorMessage>{errors.childAge?.message}</ErrorMessage>}
-       
-            <InputStyled type="time" {...register("time")} />
+            {errors.childAge && (
+              <ErrorMessage>{errors.childAge?.message}</ErrorMessage>
+            )}
+          </InputWrapBox>
+          <InputWrapBox>
+            <InputStyled
+              onClick={() => setIsOpenTimeDrop(true)}
+              type="text"
+              {...register("time")}
+              value={timeValue}
+            />
             {errors.time && <ErrorMessage>{errors.time?.message}</ErrorMessage>}
-         
-        </InputAppointmentBox>
-        <InpunFullAppoinmentBox>
+            {isOpenTimeDrop && (<TimeListStyledBox>
+              <h3>Meeting time</h3>
+              <TimeListStyled onClick={handleTimeCheck}>
+                <li>00 : 00</li>
+                <li>00 : 30</li>
+                <li>01 : 00</li>
+                <li>01 : 30</li>
+                <li>02 : 00</li>
+                <li>02 : 30</li>
+                <li>03 : 00</li>
+                <li>03 : 30</li>
+                <li>04 : 00</li>
+                <li>04 : 30</li>
+                <li>05 : 00</li>
+                <li>05 : 30</li>
+                <li>06 : 00</li>
+                <li>06 : 30</li>
+                <li>07 : 00</li>
+                <li>07 : 30</li>
+                <li>08 : 00</li>
+                <li>08 : 30</li>
+                <li>09 : 00</li>
+                <li>09 : 30</li>
+                <li>10 : 00</li>
+                <li>10 : 30</li>
+                <li>11 : 00</li>
+                <li>11 : 30</li>
+                <li>12 : 00</li>
+                <li>12 : 30</li>
+                <li>13 : 00</li>
+                <li>13 : 30</li>
+                <li>14 : 00</li>
+                <li>14 : 30</li>
+                <li>15 : 00</li>
+                <li>15 : 30</li>
+                <li>16 : 00</li>
+                <li>16 : 30</li>
+                <li>17 : 00</li>
+                <li>17 : 30</li>
+                <li>18 : 00</li>
+                <li>18 : 30</li>
+                <li>19 : 00</li>
+                <li>19 : 30</li>
+                <li>20 : 00</li>
+                <li>20 : 30</li>
+                <li>21 : 00</li>
+                <li>21 : 30</li>
+                <li>22 : 00</li>
+                <li>22 : 30</li>
+                <li>23 : 00</li>
+                <li>23 : 30</li>
+              </TimeListStyled>
+            </TimeListStyledBox>)}
+          </InputWrapBox>
           <InputStyled
             type="email"
             {...register("email")}
@@ -112,7 +206,7 @@ const AppointmentModal = ({ open, onClose }) => {
             placeholder="Comment"
           />
           <ErrorMessage>{errors.comment?.message}</ErrorMessage>
-        </InpunFullAppoinmentBox>
+        </InputAppointmentBox>
         <Button>Send</Button>
       </FormStyled>
     </ModalWindow>
--- src/components/AppointmentModal/AppointmentModal.styled.js
+++ src/components/AppointmentModal/AppointmentModal.styled.js
@@ -31,14 +31,54 @@ export const InputAppointmentBox = styled.div`
     flex-wrap: wrap;
     column-gap: 8px;
     row-gap: 16px;
-
-
-`;
-
-export const InpunFullAppoinmentBox = styled.div`
-    display:flex;
-    flex-direction: column;
-    gap: 16px;
-    margin-top: 16px;
     margin-bottom: 40px;
+
 `;
+
+export const InputWrapBox = styled.div`
+    position: relative;
+   width: calc((100% - 8px) / 2);
+   p{
+    margin: 0;
+   }
+`;
+
+// export const InputStyledTime = styled(InputStyled)`
+//     position: relative;
+// `;
+
+export const TimeListStyledBox = styled.div`
+    position: absolute;
+    width: 151px;
+    height: 180px;
+    border-radius: 12px;
+    box-shadow: 0 20px 69px 0 rgba(0, 0, 0, 0.07);
+    background-color: #fff;
+    right:0;
+    top: 68px;
+    padding: 16px;
+`;
+
+export const TimeListStyled = styled.ul`
+    margin-top: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 108px;
+    gap: 4px;
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+      scrollbar-width: none;
+    li{
+        cursor: pointer;
+        display: flex;
+        gap: 2px;
+        line-height: 125%;
+        color: rgba(17, 16, 28, 0.3);
+        &:hover,
+        &:focus{
+            color: var(--text-black);
+        }
+    };
+`;
+
